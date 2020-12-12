55481469cb2b48bcb7bd2496663203ea6e58f4c9
filip <fpapranecx@gmail.com>
2020-12-12 16:49:07 +0100

    add gtm module

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import type { AppProps } from 'next/app'
-import React from "react";
+import React, { useEffect } from "react";
 import 'styles/import.scss'
+import TagManager, { TagManagerArgs } from 'react-gtm-module'
+import { isProduction } from "utils/helpers";
+
+const gtmParams: TagManagerArgs = {
+  gtmId: 'GTM-P7P463Q'
+}
 
 function MyApp({Component, pageProps}: AppProps) {
+  useEffect(() => {
+    if (isProduction()) {
+      TagManager.initialize(gtmParams)
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }
 
